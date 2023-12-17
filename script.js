82e63e292a6440d375081b052922cdd95d0442df
Soo <li_xiuheng@hotmail.com>
2023-12-17 22:53:03 +0000

    modified conditions in getPasswordOptions function

--- script.js
+++ script.js
@@ -88,84 +88,44 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-
+var mychoice = lowerCasedCharacters
 
 // Function to prompt user for password options
 function getPasswordOptions(option) {
-    confirm('Click Ok to confirm including ' + option + '.');
-    switch (option) {
-        case 'special characters':
-        text = 'specialCharacters';
-        break;
-        case 'numeric characters':
-        text = 'numericCharacters';
-        break;
-        case 'uppercase characters':
-        text = 'upperCasedCharacters';
-        break;}
-    return mychoice.concat(option) 
+    return mychoice.push(...option) 
 }
-// need prompt("How many characters would you like your password to contain?")
-//if the text.length (var password) <= 8 or > 128, return error message "Password length must be provided as a number"
+// Set up error messages to stick to the logic
 const input = prompt("How many characters would you like your password to contain?")
 if (input < 8 || input > 128) {
     alert('Password length must be at least 8 characters but no more than 128. Refresh to try again.')}
     else if (isNaN(input)) {
         alert('Password length must be provided as a number. Refresh to try again.')}
-   
-
-var mychoice = lowerCasedCharacters
-var newchoice
-if (getPasswordOptions('special characters')) {
-    var newchoice = mychoice.concat(specialCharacters)
-}
-if (getPasswordOptions('numeric characters')) {
-    var newchoice = mychoice.concat(numericCharacters)
-}
-if (getPasswordOptions('uppercase characters')) {
-    var newchoice = mychoice.concat(upperCasedCharacters)
-}
-
-
-// var array = getPasswordOptions()
-// switch (array) {
-//     case getPasswordOptions('special characters'):
-//         array += specialCharacters
-//         break;
-//     case getPasswordOptions('numeric characters'):
-//             array += numericCharacters
-//             break;
-//     case getPasswordOptions('uppercase characters'):
-//                 array += upperCasedCharacters
-//                 break;
-// }
+    else {
+        if (confirm('Click Ok to confirm including special characters.')) {
+        getPasswordOptions(specialCharacters)
+        }
+        if (confirm('Click Ok to confirm including numeric characters.')) {
+            getPasswordOptions(numericCharacters)
+        }
+        if (confirm('Click Ok to confirm including uppercase characters.')) {
+            getPasswordOptions(upperCasedCharacters)
+        }
+    }
 // Function for getting a random element from an array
-// if (getPasswordOptions('special characters')) {
-//     var array = specialCharacters
-// }
-
-// if (getPasswordOptions('numeric characters')) {
-//     var array = numericCharacters
-// }
-
-// if (getPasswordOptions('uppercase characters')) {
-//     var array = upperCasedCharacters
-// }
-
 function getRandom(array) {
     var password = ''
 for (i = 0; i < input; i++) {
-    var char = Math.floor(Math.random() * newchoice.length);
+    var char = Math.floor(Math.random() * mychoice.length);
     password += array[char]
 }
 return password
 }
 
 
-
 // Function to generate password with user input
+alert('Your password is now created. Please click "Generate Password"!')
 function generatePassword() {
-    return getRandom(newchoice)
+    return getRandom(mychoice)
 }
 
 // Get references to the #generate element
@@ -178,6 +138,7 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
